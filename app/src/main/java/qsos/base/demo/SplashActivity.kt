2040771f897245b00d531bdfbac22dc0d25aca87
华清松 <821034742@qq.com>
2019-10-29 18:36:51 +0800

    修复闪屏用户获取展示BUG

--- app/src/main/java/qsos/base/demo/SplashActivity.kt
+++ app/src/main/java/qsos/base/demo/SplashActivity.kt
@@ -95,7 +95,7 @@ class SplashActivity(
                                     }
                             )
                         }
-                    }, false)
+                    }, true)
         })
     }
 
